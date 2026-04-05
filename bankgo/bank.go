e3e6c09acad6490003206bffbfc2da21af931ec1
Sarfaraj Ansari <imsarfaraaj7@gmail.com>
2026-04-05 12:39:18 +0530

    added switch and for loop in placeof whilloopa thereis no while loop

--- bankgo/bank.go
+++ bankgo/bank.go
@@ -1,42 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var accountBalance float64 = 10000.00
-	fmt.Println("************WELCOME TO GO BANK*************")
-	fmt.Println("What do you wana do ")
-	fmt.Println("1.Check Balance")
-	fmt.Println("2.Deposit Money")
-	fmt.Println("3.Withdraw Money")
-	fmt.Println("4.Exit")
 	var choice int32
-	fmt.Println("Please Select Your choice :")
-	fmt.Scan(&choice)
-	fmt.Printf("You Have Selectd %d \n", choice)
-	if choice == 1 {
-		fmt.Printf("Your Balance is : %.2f \n", accountBalance)
-	} else if choice == 2 {
-		fmt.Println("Enter Money You wanna deposit")
-		var moneyToBeDeposited float64
-		fmt.Scan(&moneyToBeDeposited)
-		if moneyToBeDeposited > 0 {
-			accountBalance += moneyToBeDeposited
-			fmt.Printf("New Balanace is : %.3f", accountBalance)
-		} else {
-			fmt.Println("Money to be Deposited Should be Greater Than Zero !")
+	for choice != 4 {
+		fmt.Println("************WELCOME TO GO BANK*************")
+		fmt.Println("What do you wana do ")
+		fmt.Println("1.Check Balance")
+		fmt.Println("2.Deposit Money")
+		fmt.Println("3.Withdraw Money")
+		fmt.Println("4.Exit")
+
+		fmt.Println("Please Select Your choice :")
+		fmt.Scan(&choice)
+		fmt.Printf("You Have Selectd %d \n", choice)
+		switch choice {
+
+		case 1:
+			fmt.Printf("Your Balance is : %.2f \n", accountBalance)
+		case 2:
+			fmt.Println("Enter Money You wanna deposit")
+			var moneyToBeDeposited float64
+			fmt.Scan(&moneyToBeDeposited)
+			if moneyToBeDeposited > 0 {
+				accountBalance += moneyToBeDeposited
+				fmt.Printf("New Balanace is : %.3f", accountBalance)
+			} else {
+				fmt.Println("Money to be Deposited Should be Greater Than Zero !")
+			}
+
+		case 3:
+			var withdrawAmount float64
+			fmt.Println("Enter Money You Wnna Withdraw !")
+			fmt.Scan(&withdrawAmount)
+			if withdrawAmount > accountBalance {
+				fmt.Println("You Dont Have Enough Amount !")
+			} else {
+				accountBalance -= withdrawAmount
+				fmt.Printf("Succesfull and New AMount Balance is : %.2f", accountBalance)
+			}
+		default:
+			fmt.Println("Thank you !")
+			return
 		}
-	} else if choice == 3 {
-		var withdrawAmount float64
-		fmt.Println("Enter Money You Wnna Withdraw !")
-		fmt.Scan(&withdrawAmount)
-		if withdrawAmount > accountBalance {
-			fmt.Println("You Dont Have Enough Amount !")
+		fmt.Println("******** Do You Wanna Continue! (y/n) **********")
+		var userInput string
+		fmt.Scan(&userInput)
+
+		if userInput == "y" {
+			continue
+		} else if userInput == "n" {
+			fmt.Println("Exiting...")
+			break
 		} else {
-			accountBalance -= withdrawAmount
-			fmt.Printf("Succesfull and New AMount Balance is : %.2f", accountBalance)
+			fmt.Println("Invalid input, please enter y or n")
 		}
-	} else {
-		fmt.Println("Thank you !")
 	}
 }
